257464e080d8aa84805e95fe8ea7404e061f4473
Yunus Khan <94840104+yunuskkhan@users.noreply.github.com>
2021-12-20 16:05:03 +0300

    Update bring.js

--- bring.js
+++ bring.js
@@ -10,20 +10,35 @@ class bring{
     constructor(listOfLibraries){
         this.listOfLibraries = [];
     }
-    addLibrary(library,src){
-        var data = {library:library,src:src};
+    addLibrary(library,src,type){
+        var data = {library:library,src:src,type:type};
         this.listOfLibraries.push(data);
     }
     bring(library,locatedIn){
         const head = document.querySelector('head');
         const end = document.getElementsByTagName('body')[0];
- 
+        // console.log(this.listOfLibraries);
         this.listOfLibraries.forEach(element => {   
             if(element.library == library){
+                var link = '';
+                console.log(element.type);
+                if(element.type=='link'){
+                    link = 'href';
+                }else if(element.type=='script'){
+                    link = 'src';
+                }
                 if(locatedIn == 'head'){
-                    head.innerHTML += element.src;
-                }else{
-                    end.innerHTML += element.src;
+                    var els = document.createElement(element.type);
+                    els.setAttribute(link,element.src);
+                    if(element.type=='link'){
+                        els.setAttribute('rel','stylesheet');
+                    }
+                    head.appendChild (els);
+                }else if(locatedIn == 'end'){
+                    var el = document.createElement(element.type);
+                    el.setAttribute(link,element.src);
+                    // console.log(element);
+                    end.appendChild(el);
                 }
             }
         });
@@ -33,11 +48,15 @@ class bring{
 var brng = new bring();
 
 // addLibrary() function is used to add custom library list so that to include sspecific library by Library name using bring() functoin
-brng.addLibrary('bootstrap','<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">');
-brng.addLibrary('bootstrapIcons','<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">');
-brng.addLibrary('jq','<script src="https://code.jquery.com/jquery-3.6.0.min.js" ></script>');
+brng.addLibrary('bootstrap','https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css','link');
+brng.addLibrary('bootstrapIcons','https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css','link');
+brng.addLibrary('jq','https://code.jquery.com/jquery-3.6.0.min.js','script');
+brng.addLibrary('apps','apps.js','script');
+brng.addLibrary('pos_app','pos_app.js','script');
 
 // call bring() function to identify the Library name and assign its src link to either head or end of the doc before </body> tag
 brng.bring('bootstrap','head');
 brng.bring('bootstrapIcons','head');
 brng.bring('jq','end');
+brng.bring('pos_app','end');
+brng.bring('apps','end');
